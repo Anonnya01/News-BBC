7737cb68226736b6eec7016db6b225d47ed65840
Anonnya01 <anonnya1999@gmail.com>
2025-09-06 00:45:07 +0600

    News added by category

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// ---------category container--------//
+// -1️⃣✅--------category container--------//
 const categoryContainer = document.getElementById("topics");
+
+// 2️⃣-✅---------news container----------//
+const newsContainer =document.getElementById("newsBox")
 
 // ----------get Category-----------//
 const loadCategory = () => {
@@ -36,10 +39,48 @@ const showCategory = (categories) => {
     });
     // ----adding----//
     if (e.target.localName === "li") {
-      console.log(e.target);
+      //   console.log(e.target);
       e.target.classList.add("border-b-4");
+      loadNews(e.target.id); //---calling load news⭐⭐
     }
   });
 };
-// ------------show category delegation added----//
+// ------------show category delegation added & END----//
+
+// -----------get News------------//
+
+const loadNews = (topicId) => {
+  console.log(topicId);
+
+  fetch(`https://news-api-fs.vercel.app/api/categories/${topicId}`)
+  .then(res=>res.json())
+  .then(data =>{
+    //   console.log(data.articles);
+      showNews(data.articles) 
+  })
+  .catch(err=>{
+    console.log(err);
+  })
+};
+
+// -----------show News-----------//
+  const showNews=(articles)=>{
+   newsContainer.innerHTML=''
+  articles.forEach(article =>{
+   newsContainer.innerHTML +=`
+   <div>
+      
+      <div>
+      <img src="${article.image.srcset[5].url}">
+      </div>
+
+    <h1>${article.title}</h1>
+   </div>
+   `
+  })
+
+  }
+
+
+
 loadCategory();
